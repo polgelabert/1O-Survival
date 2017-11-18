ed7e56acc07038640f57c99ced2d907ea07a46e4
polgelabert <polgelabert@gmail.com>
2017-11-18 23:42:46 +0100

    idea

---
+++ src/main/java/Vista/Main.java
@@ -0,0 +1,23 @@
+package Vista;
+
+import Modelo.Player;
+
+public class Main {
+
+
+     public static void main(String[] args){
+
+
+
+        Player p = new Player();
+        p.setVida(50);
+        Integer res = p.getVida() - 40;
+        System.out.println(res);
+
+
+
+    }
+
+
+
+}
